fc1a8960aa6f43699456b340fb14852ecc62a6ea
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-05-06 12:41:06 +1000

    add affirmation api

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 export const returnTrue = () => true
+
+// entry point for affirmation url
+const affirmationURL = "https://cors-anywhere.herokuapp.com/https://www.affirmations.dev/"
+// initially set data to loading. It can be something else, or an empty string.
+let data = "loading"
+// async function to fetch data from affirmation api
+async function getAffirmation () {
+        let response = await fetch(affirmationURL)
+        data = await response.json()
+        return data.affirmation
+}
