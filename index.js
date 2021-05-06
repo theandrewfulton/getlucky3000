af34f9522adc1d2ec30dcaeb5543993c437ec3d9
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-05-06 22:34:38 +1000

    dog api function now takes breed arguments

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
 export const returnTrue = () => true
 
 // URLs for API entry points. affirmationURL runs through proxy to avoid CORS error
 const affirmationURL = 'https://cors-anywhere.herokuapp.com/https://www.affirmations.dev/'
 const corporateBsURL = 'https://corporatebs-generator.sameerkumar.website/'
 const dadJokeFetch = 'https://us-central1-dadsofunny.cloudfunctions.net/DadJokes/random/jokes'
-const dogPic = 'https://dog.ceo/api/breeds/image/random'
+const dogPic = 'https://dog.ceo/api/breed'
 
 // async function to fetch data from affirmation api
 // this then calls a function called updateAffirmation and passes the affirmation data do it
@@ -23,7 +22,7 @@ async function getCorporateBS () {
 }
 
 
-// Dad joke
+Dad joke
 fetch(dadJokeFetch)
     .then((response) => response.json()) //returns a promise
     .then((data) => {console.log(data)}) //handles the data and runs a function
@@ -44,22 +43,13 @@ fetch(dadJokeFetch)
     }
 
 
-let doggie = document.getElementById("breed")
-fetch(dogPic)
-    .then((response) => response.json()) //returns a promise
-    .then((data) => {console.log(data)}) //handles the data and runs a function
-    .catch((error) => {console.log("error: " + error)}) 
-
-
-
-async function getDogPic() {
-    let data = null
-    let response = await fetch(`${dogPic}`)
-    data = await response.json()
-    doggie.src = data.message
-
+async function getDog(breed) {
+    let response = await fetch(`${dogPic}/${breed}/images/random`)
+    dogData = await response.json()
+    updateDog = dogData.message
+    // change console.log with your function to update the html content
+    console.log(updateDog)
 }
-
 
 
 
@@ -69,8 +59,13 @@ async function getDogPic() {
 // let test_img = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.rspcansw.org.au%2Fwhat-we-do%2Fadoptions%2Fdogs-and-puppies%2F&psig=AOvVaw3fTLDUiJGBwJutJInTh37F&ust=1620352875297000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLiEhoD7s_ACFQAAAAAdAAAAABAD'
 
 
+
+
+
+
 // testing
-getDogPic()
+getDogPic("collie")
+console.log(doggie.src)
 getAffirmation()
 getCorporateBS()
 function updateAffirmation(newAffirmation) {
@@ -81,27 +76,4 @@ function updateBS(newBS) {
     corporateBS = document.getElementById("bs")
     corporateBS.innerHTML = (newBS)
 }
-
-
-let subButton = document.getElementById("submit")
-
-
-let nameTag = document.getElementById("name")
-let ageTag = document.getElementById("age")
-let occupationTag = document.getElementById("occupation")
-let breedTag = document.getElementById("breed")
-
-
-
-subButton.addEventListener("click", function(event) {
-    event.preventDefault()
-    let nameValue = nameTag.value
-    let ageValue = ageTag.value
-    let occupationValue = occupationTag.value
-    let breedValue = breedTag.value
-        //This is just for testing not needed
-        //let para = document.getElementById("test")
-        //Uncomment and test to see if above values work
-        //para.innerText = `name:${nameValue}, age:${ageValue}, occupation:${occupationValue}, breed:${breedValue} `
-})
-
+getDog("collie")
